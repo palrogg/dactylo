89e50f7f3cae9fb07e469a4428fc50eaaf55a49e
Paul Ronga <gen@paulronga.ch>
2025-07-18 15:56:58 +0200

    draft text api

---
+++ user_API/database/migrations/2025_07_18_132303_create_texts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('texts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('author');
+            $table->integer('publication_year')->nullable()->default(null); // only for books
+            $table->text('content');
+            $table->date('display_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('texts');
+    }
+};
